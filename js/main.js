ba0017c4fe668c67ebf0d0a5451cb4e9042513be
nathymellal <72868466+nathymellal@users.noreply.github.com>
2021-03-30 11:27:05 +0200

    fetch

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const loadData = heroes => {
+    console.log(heroes) // write your code using the data in a function
+    // note that you can not access heroes before this function is called.
+  }
+  
+  // Request the file fetch, it will download it in your browser cache
+  fetch('https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json')
+    .then((response) => response.json()) // parse the response from JSON
+    .then(loadData) // .then will call the function with the JSON value
