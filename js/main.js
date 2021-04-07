b52b03f8c4d0d865290a0b011d09292e81ce87ec
EPINOT <enzo.pinot@ynov.com>
2021-04-08 00:14:20 +0200

    update sort
    add fonctionality to sort function

--- js/main.js
+++ js/main.js
@@ -1,23 +1,33 @@
 let body = document.querySelector('body');
+let allJson;
 let maximumShownHero = 20; // value of last hero's id that can be displayed (20 by default)
 let minimumShownHero = 0;   // value of first hero's id that can be displayed (20 by default)
 
-// let
-
-
-
-
+// let 
 const loadData = heroes => {
     console.log(heroes) // write your code using the data in a function
     // note that you can not access heroes before this function is called.
 
+    allJson = [...heroes];
+    display()
+}
+  
+// Request the file fetch, it will download it in your browser cache
+fetch('https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json')
+.then((response) => response.json()) // parse the response from JSON
+.then(loadData) // .then will call the function with the JSON value
+
+
+const display = () => {
     let tableEl = document.querySelector('#table_heroes')
 
-    for(let i = 0; i < heroes.length; i++) {
+    for(let i = 0; i < allJson.length; i++) {
         // console.log(elOfHeroes)
+        // allHeroesElements[i].remove()
+        // console.log(heroes)
 
         // let heroCarac = [elOfHeroes.images.xs, elOfHeroes.name, elOfHeroes.biography, elOfHeroes.powerstats, elOfHeroes.appearance]
-        let heroCarac = [heroes[i].images.xs, heroes[i].name, heroes[i].biography.fullName, heroes[i].powerstats, heroes[i].appearance.race, heroes[i].appearance.gender, heroes[i].appearance.height, heroes[i].appearance.weight, heroes[i].biography.placeOfBirth, heroes[i].biography.alignment]
+        let heroCarac = [allJson[i].images.xs, allJson[i].name, allJson[i].biography.fullName, allJson[i].powerstats, allJson[i].appearance.race, allJson[i].appearance.gender, allJson[i].appearance.height, allJson[i].appearance.weight, allJson[i].biography.placeOfBirth, allJson[i].biography.alignment]
         // console.log(heroCarac)
 
         let heroEl = document.createElement('tr')
@@ -30,49 +40,39 @@ const loadData = heroes => {
             if(necessaryData == heroCarac[0]) {
                 contentTd = document.createElement('img')
                 contentTd.setAttribute("src", necessaryData)
-                tdEl.appendChild(contentTd)
+                tdEl.appendChild(contentTd)   
 
             } else if (necessaryData == heroCarac[3]) {
-
+                
                 tdEl.className = "powers"
 
                 for (powerKey in necessaryData) {
                     contentTd = document.createElement('span')
 
                     let text = document.createTextNode(`${powerKey} : ${necessaryData[powerKey]}`)
-
+                    
                     contentTd.appendChild(text);
-
+                    
                     tdEl.append(contentTd)
                 }
 
             } else {
-                contentTd = document.createTextNode(necessaryData);
-                tdEl.appendChild(contentTd)
+                contentTd = document.createTextNode(necessaryData);                           
+                tdEl.appendChild(contentTd)            
             }
-
+            
             heroEl.append(tdEl)
-
+            
         }
 
         heroEl.setAttribute("id", i)
 
         tableEl.append(heroEl)
-
+           
     }
 
     pagination()
-
 }
-
-
-
-  // Request the file fetch, it will download it in your browser cache
-fetch('https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json')
-    .then((response) => response.json()) // parse the response from JSON
-    .then(loadData) // .then will call the function with the JSON value
-
-// console.log(superHeroes)
 
 let selectNbrOfEl = document.querySelector('select');
 let choice = selectNbrOfEl.selectedIndex
@@ -88,7 +88,7 @@ selectNbrOfEl.addEventListener('click', () => {
 })
 
 
-function pagination(valueOfBtn) {
+const pagination = (valueOfBtn) => {
 
     console.log(selectNbrOfEl.options[choice].value)
     console.log(valueOfBtn)
@@ -118,8 +118,8 @@ function pagination(valueOfBtn) {
 
         // console.log(eachHero)
         if(eachHero.id >= minimumShownHero && eachHero.id <= maximumShownHero) {
-            console.log("all is fine for " + eachHero.id)
-            eachHero.style.display = ""
+            // console.log("all is fine for " + eachHero.id)
+            eachHero.style.display = ""         
         } else {
             eachHero.style.display = "none"
         }
@@ -127,26 +127,168 @@ function pagination(valueOfBtn) {
 
 }
 
-// search bar
-
 const search = () => {
     let searching = document.getElementById('searching');
     let heroElements = document.querySelectorAll('tr:not(tr#headbar)')
 
     // console.log(searching.value);
 
-    let regex = RegExp(searching.value.toUpperCase(), 'g');
+    let regex = RegExp(searching.value.toUpperCase());
 
     for (el of heroElements) {
-
+        
         if (regex.test(el.children[1].textContent.toUpperCase()) == true) {
             // console.log(el.children[1].textContent)
             el.style.display = ""
         } else {
             el.style.display = "none"
+            console.log(el.children[1].textContent.toUpperCase())
+            console.log(searching.value.toUpperCase())
         }
-    }
+    }    
 
 }
 
 
+const toSort = (toSortingElement) => {
+    // toSortingElement = sortingHTMLElement
+    // console.log(toSortingElement)
+
+    console.log('tout va bien tqt')
+
+    let category = toSortingElement.getAttribute('category');
+    let elementAttribute = toSortingElement.getAttribute('element');
+    let isAlreadyClicked = toSortingElement.getAttribute('is-already-clicked');
+    let allHeroesElements = document.querySelectorAll('tr:not(tr#headbar)');
+    let allElementOfHeadBar = document.querySelectorAll('th');
+    // console.log(isAlreadyClicked)
+
+    // console.log(allJson[1]["biography"]["fullName"])
+
+    // if (isAlreadyClicked == "false") {
+    //     allJson.sort((a,b) => {
+    //         return (a[elementAttribute] > b[elementAttribute])?1:-1;
+    //     });
+
+    //     toSortingElement.setAttribute('is-already-clicked', "true")
+    //     // console.log(toSortingElement.getAttribute('is-already-clicked'))
+
+    //     // console.log(heroes[1][elementAttribute])
+        
+    // } else {
+    //     allJson.sort((a,b) => {
+    //         return (a[elementAttribute] < b[elementAttribute])?1:-1;
+    //     });
+
+    //     toSortingElement.setAttribute('is-already-clicked', "false")
+    //     console.log(allJson)
+    //     // console.log(toSortingElement.getAttribute('is-already-clicked'))
+        
+    // }
+
+    // if (isAlreadyClicked == "false") {
+    //     allJson.sort((a,b) => {
+    //         return (a[elementAttribute] > b[elementAttribute])?1:-1;
+    //     });
+
+    //     toSortingElement.setAttribute('is-already-clicked', "true")
+    //     // console.log(toSortingElement.getAttribute('is-already-clicked'))
+
+    //     // console.log(heroes[1][elementAttribute])
+        
+    // } else {
+    //     allJson.sort((a,b) => {
+    //         return (a[elementAttribute] < b[elementAttribute])?1:-1;
+    //     });
+
+    //     toSortingElement.setAttribute('is-already-clicked', "false")
+    //     console.log(allJson)
+    //     // console.log(toSortingElement.getAttribute('is-already-clicked'))
+        
+    // }
+
+    if (category == null) {
+        if (isAlreadyClicked == "false") {
+            allJson.sort((a,b) => {
+                return (a[elementAttribute] > b[elementAttribute])?1:-1;
+            });
+    
+            toSortingElement.setAttribute('is-already-clicked', "true")
+            // console.log(toSortingElement.getAttribute('is-already-clicked'))
+    
+            // console.log(heroes[1][elementAttribute])
+            
+        } else {
+            allJson.sort((a,b) => {
+                return (a[elementAttribute] < b[elementAttribute])?1:-1;
+            });
+    
+            toSortingElement.setAttribute('is-already-clicked', "false")
+            console.log(allJson)
+            // console.log(toSortingElement.getAttribute('is-already-clicked'))
+            
+        }
+    } else {
+        if (isAlreadyClicked == "false") {
+
+            allJson.sort((a,b) => {
+
+                if (Array.isArray(a[category][elementAttribute])) {
+                    return a[category][elementAttribute][0].localeCompare(b[category][elementAttribute][0], undefined, {numeric: true})
+                }
+
+                if (a[category][elementAttribute] == "" || a[category][elementAttribute] == "-") {
+                    return 1;
+                } else if (b[category][elementAttribute] == "" || b[category][elementAttribute] == "-") {
+                    return -1;
+                }
+
+                return (a[category][elementAttribute] > b[category][elementAttribute])?1:-1;
+            });
+    
+            toSortingElement.setAttribute('is-already-clicked', "true")
+            // console.log(toSortingElement.getAttribute('is-already-clicked'))
+    
+            // console.log("tesssst" + allJson[1][category][elementAttribute][1].split(' ')[0])
+            // parseInt(a[category][elementAttribute][0].split(' ')[0]) //pas trop mal pour weight
+            // console.log("teeeeesssssssssssst " + parseInt(allJson[1][category][elementAttribute][562]))
+            
+        } else {
+
+            allJson.sort((a,b) => {
+
+                if (Array.isArray(a[category][elementAttribute])) {
+                    return b[category][elementAttribute][0].localeCompare(a[category][elementAttribute][0], undefined, {numeric: true})
+                }
+
+                if (a[category][elementAttribute] == "" || a[category][elementAttribute] == "-") {
+                    return 1;
+                } else if (b[category][elementAttribute] == "" || b[category][elementAttribute] == "-") {
+                    return -1;
+                }
+
+                return (a[category][elementAttribute] < b[category][elementAttribute])?1:-1;
+            });
+    
+            toSortingElement.setAttribute('is-already-clicked', "false")
+            console.log(allJson)
+            // console.log(toSortingElement.getAttribute('is-already-clicked'))
+            
+        }
+    }
+
+
+
+    for (el of allHeroesElements) {
+        el.remove();
+    }
+
+    for (th of allElementOfHeadBar) {
+        if(th != toSortingElement) {
+            th.setAttribute('is-already-clicked', "false");
+        }      
+    }
+
+    display()
+
+}
